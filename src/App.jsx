e5b2bcff0de477c2c5769ff542b389123f6c88fb
ar-alamin <aralamin.pi@yahoo.com>
2023-05-19 07:55:02 +0600

    React-Router-DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import ClickCounter from './HOC/ClickCounter'
-import HoverCounter from './HOC/HoverCounter'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Router/Home'
+import About from './Router/About'
+import NavBar from './Router/NavBar'
 
 const App = () => {
   return (
     <>
-      <ClickCounter />
-      <HoverCounter />
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
     </>
   )
 }
---
+++ src/Router/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
